ecf19265e67130099832ee1b5988eeb6f9bf4016
larry barrera <larrybarrera0066@gmail.com>
2025-11-17 11:31:37 -0800

    Initial Commit

---
+++ youareawesome/youareawesomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  youareawesomeApp.swift
+//  youareawesome
+//
+//  Created by BARRERA, LARRY on 11/4/25.
+//
+
+import SwiftUI
+
+@main
+struct youareawesomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ youareawesomeTests/youareawesomeTests.swift
@@ -0,0 +1,17 @@
+//
+//  youareawesomeTests.swift
+//  youareawesomeTests
+//
+//  Created by BARRERA, LARRY on 11/4/25.
+//
+
+import Testing
+@testable import youareawesome
+
+struct youareawesomeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
