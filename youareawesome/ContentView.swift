f6b6929f23523b82d51188da8b6a84a20cb0a8e6
larry barrera <larrybarrera0066@gmail.com>
2025-11-19 11:28:32 -0800

    larry

--- youareawesome/ContentView.swift
+++ youareawesome/ContentView.swift
@@ -10,42 +10,55 @@ import SwiftUI
 struct ContentView: View {
     @State private var message = ""
     @State private var imageName = ""
-    @State private var imageNumber = 0
-    @State private var messageNumber = 0
+    @State private var lastMessageNumber = -1 //lastMessageNumber will nevr be -1
+    @State private var lastImageNumber = -1
     var body: some View {
         VStack {
-            Spacer()
-            
-            Image(imageName)
-                .resizable()
-                .scaledToFit()
-                .clipShape(RoundedRectangle(cornerRadius:30))
-                .shadow(radius:30)
             
             Text(message)
                 .font(.largeTitle)
                 .fontWeight(.heavy)
                 .foregroundStyle(.red)
                 .multilineTextAlignment(.center)
+                .minimumScaleFactor(0.5)
+                .frame(height: 100)
+                .animation(.easeInOut(duration: 0.15), value: message)
+
+            
+            Image(imageName)
+                .resizable()
+                .scaledToFit()
+                .clipShape(RoundedRectangle(cornerRadius:30))
+                .shadow(radius:30)
+                .animation(.default, value: imageName)
+        
+            
             Spacer()
             
             
             Button("Show Message") {
-                let messages = ["You Are Awesome!","You Are Great!","You Are Fantastic!","Fabulous? That's You","You Make Me Smile!","When the Genius Bar Needs Help, They Call You"]
-                
+                let messages = ["You Are Awesome!",
+                            "When the Genius Bar Needs Help, They Call You","You Are Great!","You Are Fantastic!","Fabulous? That's You","You Make Me Smile!",]
+            
+//generate a random messageNumber to use as an index
+                //if messageNumber == lastMessageNumber { // keep generating a new random messageNumber
+    //until you get a messageNumber != lastMessageNumber
+    //set messageString to messages[messageNumber]
+    //update the lastMessageNumber with messageNumber
+    
+                var messageNumber: Int
+                repeat{
+                    messageNumber = Int.random(in:0...messages.count-1)
+                }while messageNumber == lastMessageNumber
                 message = messages[messageNumber]
-                messageNumber += 1
-                if messageNumber == messages.count{
-                    messageNumber = 0
+                lastImageNumber = messageNumber
+                
+                var imageNumber = Int.random(in:0...9)
+              while imageNumber == lastImageNumber {
+                    imageNumber = Int.random(in:0...9)
                 }
                 imageName = "image\(imageNumber)"
-                
-                //imageNumber = imageNumber + 1
-                
-                imageNumber += 1
-                if imageNumber > 9{
-                    imageNumber = 0
-                }
+                lastImageNumber = imageNumber
             }
             
             .buttonStyle(.borderedProminent)
